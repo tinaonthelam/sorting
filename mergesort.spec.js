fe9a5713e065611bb9b1b4c41dc15e640234340a
Tina Lam <tina.lam516@gmail.com>
2017-03-02 17:08:47 -0500

    final

--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,20 +1,25 @@
-describe('Split Array function', function() {
-  it('returns an array containing two arrays', function() {
-    expect( split([])).toEqual([ [], [] ])
+describe('Merge Sort Functions', function() {
+  describe('Split Array', function() {
+    it('returns an array containing two arrays', function() {
+      expect( split([])).toEqual([ [], [] ])
+    });
+    it('is able to split an array into two halves', function() {
+      expect( split([4, 2, 3, 1])).toEqual([ [4, 2], [3, 1] ])
+    });
   });
-  it('is able to split an array into two halves', function() {
-    expect( split([4, 2, 3, 1])).toEqual([ [4, 2], [3, 1] ])
-  });
-});
 
-describe('Merge Sort', function(){
-  it('returns an array', function(){
-    expect( merge([], [])).toEqual([])
+  describe('Merge', function(){
+    it('returns an array', function(){
+      expect( merge([], [])).toEqual([])
+    });
+    it('is able to merge two sorted arrays into one sorted array', function(){
+      expect( merge([1, 2, 3, 4], [5, 6, 7, 8])).toEqual([1, 2, 3, 4, 5, 6, 7, 8])
+    });
   });
-  it('is able to merge two sorted arrays into one sorted array', function(){
-    expect( merge([1, 2, 3, 4], [5, 6, 7, 8])).toEqual([1, 2, 3, 4, 5, 6, 7, 8])
-  });
-  it('is able to sort an unsorted array', function() {
+
+  describe('Merge Sort', function() {
+    it('is able to sort an unsorted array', function() {
       expect( mergeSort([3, 6, 1, 9, 2, 7])).toEqual([1, 2, 3, 6, 7, 9])
-  })
+    });
+  });
 });
