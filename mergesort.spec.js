e8618fa139fe62a080332aa9d349e3f91f2f0fdd
Tina Lam <tina.lam516@gmail.com>
2017-03-02 15:39:26 -0500

    merge

---
+++ mergesort.spec.js
@@ -0,0 +1,17 @@
+describe('Split Array function', function() {
+  it('returns an array containing two arrays', function() {
+    expect( split([])).toEqual([ [], [] ])
+  });
+  it('is able to split an array into two halves', function() {
+    expect( split([4, 2, 3, 1])).toEqual([ [4, 2], [3, 1] ])
+  });
+});
+
+describe('Merge Sort', function(){
+  it('returns an array', function(){
+    expect( merge([], [])).toEqual([])
+  });
+  it('is able to merge two sorted arrays into one sorted array', function(){
+    expect( merge([1, 2, 3, 4], [5, 6, 7, 8])).toEqual([1, 2, 3, 4, 5, 6, 7, 8])
+  });
+});
